e1844ec5775c5db146022501a1865cdb21fd6d06
Peter <peter_clark@talktalk.net>
2020-02-11 10:21:29 +0000

    Added some Unit tests

--- core/test/java/FireTruckTest.java
+++ core/test/java/FireTruckTest.java
@@ -1,41 +1,43 @@
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.sprites.Entity;
 import com.mygdx.game.sprites.Firetruck;
-import com.mygdx.game.sprites.Unit;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-//Instance of the Unit class to test on (is abstract)
-class unitForFireTruck extends Unit {
-
-    private int maxWater;
-    private int currentWater;
-    private boolean selected;
-
-    public unitForFireTruck(Vector2 position, int width, int height, Texture texture, int maxHealth, int range, Unit target,
-                            int speed, int dps, int bearing, int maxWater, boolean selected, float attackCooldown) {
-        super(position, width, height, texture, maxHealth);
-        this.maxWater = maxWater;
-        this.currentWater = maxWater;
-        this.selected = selected;
-    }
-
-}
-
-
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Graphics.class})
 public class FireTruckTest {
+    private Entity instance;
+    private boolean expected;
+
 
     //Instance of entity class to test methods on
     Entity testEntity = new Entity(new Vector2(200, 200), 100, 100, null);
 
     //Instance of the FireTruck class to test on
-    Firetruck testFireTruck = new Firetruck(new Vector2(100, 100 ), 101, 102, null,
+    Firetruck testFireTruck = new Firetruck(new Vector2(100, 100 ), 100, 100, null,
             103, 10, null, 10, 12, 13, true);
 
+    //ASSESSMENT 3
+    @Before
+    public void setup() {
+        Gdx.graphics = PowerMockito.mock(Graphics.class);
+        lenient().when(Gdx.graphics.getDeltaTime()).thenReturn(1f);
+    }
 
     //Testing basic constructor functionality with getters
     @Test
@@ -66,6 +68,34 @@ public class FireTruckTest {
         assertEquals(0, testFireTruck.getCurrentWater());
     }
 
+    //ASSESSMENT 3
+    @Test
+    public void truckShouldMoveWhenCommandedTest() {
+        testFireTruck.move(2);
+        assertEquals(testFireTruck.getPosition().x, 110);
+    }
 
+    //ASSESSMENT 3
+    @Test
+    public void movingEquallyInAllDirectionsShouldResultInOriginalPositionTest() {
+        testFireTruck.move(1);
+        testFireTruck.move(2);
+        testFireTruck.move(3);
+        testFireTruck.move(4);
+        assertEquals(testFireTruck.getPosition(), new Vector2(100,100));
+    }
 
+    //ASSESSMENT 3
+    @Test
+    public void willCollideReturnsTrueIfInRange() {
+        Entity e = new Entity(new Vector2(105, 100), 1, 1, null);
+        assertTrue(testFireTruck.willCollide(e, 2));
+    }
+
+    //ASSESSMENT 3
+    @Test
+    public void willCollideReturnsFalseIfNotInRange() {
+        Entity e = new Entity(new Vector2(100, 300), 1, 1, null);
+        assertFalse(testFireTruck.willCollide(e, 3));
+    }
 }
